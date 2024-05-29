dd6e4ad15556d61bc9a3522b0c241cf527f3edd0
Mahadeb <maitysusanta824@gmail.com>
2024-05-29 21:22:08 +0530

    Create ClassObject.java

---
+++ JavaOopsHw/ClassObject.java
@@ -0,0 +1,18 @@
+//write a java program of class and object with real life example.
+
+  class car{// this is a class 
+    String  brand = "BMW";
+    String model = " BMWB42C2A";
+    int Yob =2024;
+    
+}
+public class Main {
+	
+	public static void main(String[] args) {
+		 car obj =new car(); // this is object of class car 
+		 System.out.println(" Car brand name " + obj.brand);
+		 	 System.out.println(" Car model no " + obj.model);
+		 	 System.out.println(" Car buy of date  " + obj.brand);
+		 
+	}
+}
